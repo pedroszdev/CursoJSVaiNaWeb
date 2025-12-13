4ffb8f9a89be864c4c0dbf9e525c9276bb1b77d6
Pedro Manoel <ps6711534@gmail.com>
2025-12-12 22:35:43 -0300

    arrumando bug de deploy

--- projeto-medicos-dentistas/src/App.jsx
+++ projeto-medicos-dentistas/src/App.jsx
@@ -1,7 +1,7 @@
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import Voluntario from './pages/voluntario'
+import Voluntario from './pages/Voluntario'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
